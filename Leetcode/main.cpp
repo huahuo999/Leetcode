d574f953224e66bc6f76b8707dce50e365b8e2e9
ls <>
2021-08-10 18:55:20 +0800

    Leetcode

--- Leetcode/main.cpp
+++ Leetcode/main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+using namespace std;
+
+int partition(int array[], int low ,int high){
+    int pivot = array[low];
+    while(low<high){
+        while(array[high]>=pivot&&low<high) --high;
+        array[low] = array[high];
+        while(array[low]<pivot&&low<high) ++low;
+        array[high] = array[low];
+    }
+    array[low] = pivot;
+    return low;
+
+}
+
+void quickSort(int array[], int low ,int high){
+
+    if(low < high){
+        int pivotpos = partition(array, low, high);
+        quickSort(array, low, pivotpos-1);
+        quickSort(array, pivotpos+1, high);
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int array[5] = {2,3,4,1,5};
+    quickSort(array,0,4);
+    for(int i=0;i<5;i++)
+        cout<<"array["<<i<<"]= "<<array[i]<<endl;
     return 0;
 }
