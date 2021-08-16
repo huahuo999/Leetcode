9c1acfdaaabac831fdce24f39f836db4a780d134
ls <>
2021-08-16 15:05:56 +0800

    Leetcode

--- Leetcode/main.cpp
+++ Leetcode/main.cpp
@@ -23,10 +23,37 @@ void quickSort(int array[], int low ,int high){
     }
 }
 
+
+void Merge(int array[], int low, int mid, int high){
+    int *assist = (int *)malloc(sizeof(array));
+    int i,j,k;
+    for(k=low;k<=high;k++)
+        assist[k] = array[k];
+    for(i=low,j=mid+1,k=low;i<=mid&&j<=high;k++){
+        cout<<"k="<<k<<endl;
+        if(assist[i]<=assist[j])
+            array[k] = assist[i++];
+        else
+            array[k] = assist[j++];
+    }
+    while(i<=mid) array[k++] = assist[i++];
+    while(j<=high) array[k++] = assist[j++];
+}
+
+
+void MergeSort(int array[], int low, int high){
+    if(low<high){
+        int mid = (low+high)/2;
+        MergeSort(array, low, mid);
+        MergeSort(array, mid+1, high);
+        Merge(array, low, mid, high);
+    }
+
+}
 int main() {
-    int array[5] = {2,3,4,1,5};
-    quickSort(array,0,4);
-    for(int i=0;i<5;i++)
+    int array[8] = {8,6,2,3,4,1,5,9};
+    MergeSort(array,0, 7);
+    for(int i=0;i<8;i++)
         cout<<"array["<<i<<"]= "<<array[i]<<endl;
     return 0;
 }
